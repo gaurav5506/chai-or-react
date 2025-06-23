c2ad22f91cdef743cd76602f9e1ebd32814a9aad
gaurav5506 <ygrv13@gmail.com>
2025-06-23 16:57:11 +0530

    react

--- vite1/src/App.jsx
+++ vite1/src/App.jsx
@@ -1,8 +1,31 @@
-import Chai from "./chai"
+import {useState}from 'react';
+
 function App() {
+  // let counter=5
+  let [counter,setcounter]=useState(0)
+  let min=0;
+  let max=10;
+
+  const addvalue=()=>{
+    if(counter < max){
+    setcounter(counter+1)
+  }
+}
+  const removevalue=()=>{
+    if(counter > min){
+    setcounter(counter-1)
+  }
+}
 
   return (
-    <Chai/>
+    <>
+    <h1>A Counter App </h1>
+    <h2>counter value: {counter}</h2>
+
+    <button onClick={addvalue} disabled={counter===max}>add value</button>
+    <button onClick={removevalue} disabled={counter===min}>remove value</button>
+    <p>{counter}</p>
+    </>
   )
 }
 
