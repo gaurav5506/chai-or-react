b8c1b4b247dbc08c2bd8cfac887b5dd39a61cd01
gaurav5506 <ygrv13@gmail.com>
2025-06-24 16:47:45 +0530

    install tailwind

--- vite2/src/App.jsx
+++ vite2/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <h1 className='bg-green' >Tailwind</h1>
+  <h1 className= 'w-lg border-4 border-red-500 bg-green-400 rounded-xl p-4 text-black'>Tailwind</h1>
+
+  <div className='w-lg border-4 border-red-500 bg-green-400 rounded-xl p-4 text-black'>helllo
+    {/* <img className='w-100 ' src="https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg" alt=""/> */}
+    </div>
     </>
   )
 }
