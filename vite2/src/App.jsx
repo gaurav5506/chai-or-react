dae854e8a43bf00cdc447dc5ed83da56569e5900
gaurav5506 <ygrv13@gmail.com>
2025-06-24 21:04:03 +0530

    import cards component

--- vite2/src/App.jsx
+++ vite2/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import './index.css'
+// import './index.css'
+import Cards from './components/Cards.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +9,7 @@ function App() {
   return (
     <>
   <h1 className= 'w-lg border-4 border-red-500 bg-green-400 rounded-xl p-4 text-black'>Tailwind</h1>
-
-  <div className='w-lg border-4 border-red-500 bg-green-400 rounded-xl p-4 text-black'>helllo
-    {/* <img className='w-100 ' src="https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg" alt=""/> */}
-    </div>
+    <Cards/>
     </>
   )
 }
---
+++ vite2/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <>
+      <div className='w-lg border-4 border-red-500 bg-green-400 rounded-xl p-4 text-black'>helllo
+    <img className='w-100 ' src="https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg" alt=""/>
+    </div>
+    </>
+  )
+}
+
+export default Cards
+
