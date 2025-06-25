8fcd6556f0ba31cd3e441547d73ef78538765096
gaurav5506 <ygrv13@gmail.com>
2025-06-25 23:17:18 +0530

    d

--- vite3/src/App.jsx
+++ vite3/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import './index.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
+      <h1 className='w-lg border-4 border-red-500 bg-green-400 rounded-xl p-4 text-black'>Tailwind</h1>
     Hello world!
-  </h1>
+  
     </>
   )
 }
