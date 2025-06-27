e64e7f9f5a3bdd133a169d3e0bda390f7209293e
gaurav5506 <ygrv13@gmail.com>
2025-06-27 11:47:21 +0530

    complete vite3

--- vite3/src/App.jsx
+++ vite3/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [color, setColor] = useState("olive")
@@ -14,7 +12,11 @@ function App() {
         bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white
           px-2 py-2 rounded-3xl'>
-            <button className=''>Red</button>
+
+            <button onClick={()=>setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'red'}}>Red</button>
+            <button onClick={()=>setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'green'}}>Green</button>
+            <button onClick={()=>setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'blue'}}>Blue</button>
+
           </div>
         </div>
       </div>
